b06c61ac3f1e06491f53a5c4199f52b8c0ac2b08
Mas Eka Setiawan <mas.ekamekas@gmail.com>
2019-01-08 13:16:56 +0700

    [FIX] Slide interval

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 var nostraImageCaption = $('#nostraImages--caption');
-var nostraImageCarousel = $('#nostraImages--carousel').carousel({interval:500000});
+var nostraImageCarousel = $('#nostraImages--carousel').carousel({interval:5000});
 
 nostraImageCarousel.on('slide.bs.carousel', function(event) {
     nostraImageCaption.carousel(event.to);
